AICP/packages_apps_Launcher3

Launcher3: use actual packagename
Change-Id: If6e1ae4b1542b276628bf4834382f2b9cadb36e4

src/com/android/launcher3/qsb/QsbContainerView.java
@@ -224,7 +224,7 @@ public class QsbContainerView extends FrameLayout {
             Bundle opts = createBindOptions();
             // round quick search bar
             opts.putString("attached-launcher-identifier",
-                    getActivity().getPackageName());
+                    "com.aicp.launcher.CustomLauncher");
             opts.putString("requested-widget-style", "cqsb");
 
             Context context = getContext();

Octavi-OS/platform_packages_apps_Launcher3

src/com/android/launcher3/qsb/QsbContainerView.java
@@ -224,7 +224,7 @@ public class QsbContainerView extends FrameLayout {
             Bundle opts = createBindOptions();
             // round quick search bar
             opts.putString("attached-launcher-identifier",
-                    getActivity().getPackageName());
+                    "com.octavi.launcher.CustomLauncher");
             opts.putString("requested-widget-style", "cqsb");
 
             Context context = getContext();